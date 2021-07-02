83647a573bcc1cf729772e5bdf20a8252cc4d95e
Jesús Javier Puente <jesus.javier@bosonit.com>
2021-07-02 09:13:57 +0200

    Empezando proyecto

---
+++ src/app/person/dto/input/PersonInputDto.ts
@@ -0,0 +1,14 @@
+export class Person
+{
+    constructor (
+        public user: string,
+        public password: string,
+        public surname: string,
+        public company_email: string,
+        public personal_email: string,
+        public city: string,
+        public active: boolean,
+        public created_date: Date,
+        public termination_date: Date,
+    ) {}
+}
