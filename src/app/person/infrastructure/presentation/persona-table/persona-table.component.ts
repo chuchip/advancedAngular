0d382af0907c6ce9a5fa3d0662765663ecf6dd11
Jesús Javier Puente <jesus.javier@bosonit.com>
2021-08-03 13:26:43 +0200

    no coments

--- src/app/person/infrastructure/presentation/persona-table/persona-table.component.ts
+++ src/app/person/infrastructure/presentation/persona-table/persona-table.component.ts
@@ -9,6 +9,21 @@ import { PersonService } from 'src/app/person/services/person.service';
 })
 export class PersonaTableComponent implements OnInit {
 
+  public seasons = {
+    user: "USER",
+    password: "PASSWORD",
+    name: "NAME",
+    surname:"SURNAME",
+    company_email: "COMPANY EMAIL",
+    personal_email: "PERSONAL EMAIL",
+    city: "CITY",
+    active: "ACTIVE"  ,
+    termination_date: "TERMINATION DATE",
+    createdAt: "CREATE DATE",
+    updatedAt: "UPDATE DATE"
+  }
+  public arraySeasons:string[] =[];
+
   constructor( private _router: Router,
     private _route: ActivatedRoute, public _personService:PersonService) { 
 
@@ -16,7 +31,13 @@ export class PersonaTableComponent implements OnInit {
 
   ngOnInit(): void {
     this._personService.getAllPersons().subscribe( r => console.log(r));
+    for (var str in this.seasons)
+    {      
+      this.arraySeasons.push(this.seasons[str]);
+      
+    }
   }
 
   
 }
+ 
